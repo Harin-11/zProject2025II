013e2f55b5c94ba8c4ba4260bf446edf39e06d9d
Harin-11 <ezzie_git@proton.me>
2025-11-02 16:38:26 -0500

    log: Implementacion de Controladores y Servicios

--- Biblioteca Personal/src/main/java/com/example/service/AutorService.java
+++ Biblioteca Personal/src/main/java/com/example/service/AutorService.java
@@ -1,5 +1,64 @@
 package com.example.service;
 
-public class AutorService {
+import com.example.model.Autor;
+import com.example.repository.AutorRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+@Service
+public class AutorService implements AutorRepository {
+    private final AutorRepository autorRepository;
+
+    public AutorService(AutorRepository autorRepository) {
+        this.autorRepository = autorRepository;
+    }
+
+    public List<Autor> listarAutores() {
+        return autorRepository.findAll();
+    }
+
+    public Optional<Autor> obtenerAutorPorId(Long id) {
+        return autorRepository.findById(id);
+    }
+     
+    public Autor guardarAutor(Autor autor) {
+        return autorRepository.save(autor);
+    }
+    public Autor actualizarAutor(Long id, Autor autorActualizado) {
+        return autorRepository.findById(id)
+                .map(autor -> {
+                    autor.setNombre(autorActualizado.getNombre());
+                    return autorRepository.save(autor);
+                })
+                .orElse(null);
+    }
+    public void eliminarAutor(Long id) {
+        autorRepository.deleteById(id);
+    }
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
--- Biblioteca Personal/src/main/java/com/example/service/LibroService.java
+++ Biblioteca Personal/src/main/java/com/example/service/LibroService.java
@@ -1,5 +1,34 @@
 package com.example.service;
 
-public class LibroService {
+import com.example.model.Libro;
+import com.example.repository.LibroRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
+public class LibroService implements LibroRepository {
+    private final LibroRepository libroRepository;
 
+    public LibroService(LibroRepository libroRepository) {
+        this.libroRepository = libroRepository;
+    }
+    public List<Libro> Listar(){
+        return libroRepository.findAll();
+    }
+    public Libro guardar(Libro libro){
+        return libroRepository.save(libro);
+    }
+
+    public Libro editar(Long id, Libro LibroActualizado) {
+        Libro libro = libroRepository.findById(id).orElseThrow();
+        libro.setTitulo(libroActualizado.getTitulo());
+        libro.setAñoPublicacion(libroActualizado.getAñoPublicacion());
+        libro.setAutor(libroActualizado.getAutor());
+        libro.setCategoria(libroActualizado.getCategoria());
+        return libroRepository.save(libro);
+
+    }
+
+    public void eliminar(Long id) {
+        libroRepository.deleteById(id);
+    }
 }
