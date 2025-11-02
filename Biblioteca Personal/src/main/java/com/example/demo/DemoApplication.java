d129e5950ec13bc3e55a3886cc222f5443ec67ef
Harin-11 <ezzie_git@proton.me>
2025-11-02 18:28:56 -0500

    log-jack : Se agregaron mensajes

--- Biblioteca Personal/src/main/java/com/example/demo/DemoApplication.java
+++ Biblioteca Personal/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,21 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		mostrarMensajeBienvenida();
+	}
+	
+	private static void mostrarMensajeBienvenida() {
+		System.out.println("\n" + "=".repeat(60));
+		System.out.println("        BIBLIOTECA PERSONAL - SISTEMA DE GESTIÓN");
+		System.out.println("=".repeat(60));
+		System.out.println("Servidor: http://localhost:8071");
+		System.out.println("\nNuestros Endpoints:");
+		System.out.println("--> Libros:       http://localhost:8071/libros");
+		System.out.println("--> Autores:      http://localhost:8071/api/autores");
+		System.out.println("--> Categorías:   http://localhost:8071/api/categorias");
+		System.out.println("\nInterfaz Web:");
+		System.out.println("--> Página Principal: http://localhost:8071/");
+		System.out.println("=".repeat(60) + "\n");
 	}
 
 }
